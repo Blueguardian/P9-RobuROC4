26ea6a46a5ff6447c298844e9205b3c9550f0112
Julian <julekww@op.pl>
2024-11-05 14:05:14 +0100

    added Gazebo model

--- src/urdf_example-main/src/launch/my_rsp.launch.py
+++ src/urdf_example-main/src/launch/my_rsp.launch.py
@@ -9,11 +9,13 @@ def generate_launch_description():
 
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'urdf_example'
-    file_subpath = 'description/my_example_robot.urdf.xacro'
-    pkg_dir = os.popen('/bin/bash -c "source /usr/share/colcon_cd/function/colcon_cd.sh && \
-        colcon_cd %s && pwd"' % pkg_name).read().strip()
-    # base_path = os.path.realpath(get_package_share_directory('package')) # also tried without realpath
+    file_subpath = 'description/my_example_robot_v2.urdf.xacro'
+    # pkg_dir = os.popen('/bin/bash -c "source /usr/share/colcon_cd/function/colcon_cd.sh && \
+        # colcon_cd %s && pwd"' % pkg_name).read().strip()
+    # base_path = os.path.realpath(get_package_share_directory('urdf_example')) # also tried without realpath
     # rviz_path=base_path+'/config/config.rviz'
+    my_base_path = 'src/urdf_example-main/src/rviz'   #path to all config files
+    my_rviz_path = my_base_path+'/my_config.rviz'       #config file for rviz
 
     # Use xacro to process the file
     xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
@@ -32,19 +34,21 @@ def generate_launch_description():
         executable='joint_state_publisher',
         output='screen', # add other parameters here if required
     )    
-    # Node(
-    #         package='rviz2',
-    #         # namespace='',
-    #         executable='rviz2',
-    #         name='rviz2',
-    #         output='sreen',
-    #         arguments=['-d', str(rviz_path)]
-    # )
-            # arguments=['-d', [os.path.join(pkg_dir, 'config', 'config_file.rviz')]])
-    
-    # package='rviz2', executable='rviz2', name="rviz2", output='screen'
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen', # add other parameters here if required
+    )  
+    rviz = Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', str(my_rviz_path)]
+    )
 
-    # Run the node
+    # Run the nodes
     return LaunchDescription([
-        node_robot_state_publisher,joint_state_publisher
+        node_robot_state_publisher,joint_state_publisher,joint_state_publisher_gui,rviz
     ])
