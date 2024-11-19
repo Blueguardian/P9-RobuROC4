ade24b854bab2c09e9c30f7a47e5f29b481ae65e
Julian <julekww@op.pl>
2024-11-19 10:10:21 +0100

    Changed VLP-16 to Hi Res Spec and deleted unnecessary files

--- build/RobuROC_sim/ament_cmake_python/RobuROC_sim/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='RobuROC_sim',
-    version='0.0.0',
-    packages=find_packages(
-        include=('RobuROC_sim', 'RobuROC_sim.*')),
-)
--- build/RobuROC_sim/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/julian/model/P9-RobuROC4/install/velodyne_gazebo_plugins/share/velodyne_gazebo_plugins/package.sh"
